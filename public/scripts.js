0cc203297297971356402ad601e908931adeb876
henryque <henryque_1@hotmail.com>
2021-04-26 18:04:06 -0300

    Changing scripts(dynamical masks) -- 26/04/21

--- public/scripts.js
+++ public/scripts.js
@@ -1,17 +1,16 @@
-const input = document.querySelector('input[name="price"]');
-input.addEventListener("keydown", function(e) {
-    
-
-    setTimeout(function() {
-        let { value } = e.target;
-
+const Mask = {
+    apply(input, func) {
+        setTimeout(function() {
+            input.value = Mask[func](input.value); //Mask[func] === Mask.func
+        }, 1);
+    },
+    formatBRL(value) {
         value = value.replace(/\D/g, "");
-
-        value = new Intl.NumberFormat("pt-BR", {
+       
+        return value = new Intl.NumberFormat("pt-BR", {
             style: "currency",
             currency: "BRL"
         }).format(Number(value/100));
     
-        e.target.value = value;
-    }, 1)
-})
+    }
+}
