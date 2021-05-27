36682e9466d6914358358c4e7a8e2b5e8666dff2
henryque <henryque_1@hotmail.com>
2021-05-27 12:55:21 -0300

    PhotosUpload method(incomplete) added -- 27/05/21

--- public/scripts.js
+++ public/scripts.js
@@ -1,16 +1,32 @@
 const Mask = {
     apply(input, func) {
-        setTimeout(function() {
+        setTimeout(function () {
             input.value = Mask[func](input.value); //Mask[func] === Mask.func
         }, 1);
     },
     formatBRL(value) {
         value = value.replace(/\D/g, "");
-       
+
         return value = new Intl.NumberFormat("pt-BR", {
             style: "currency",
             currency: "BRL"
-        }).format(Number(value/100));
-    
+        }).format(Number(value / 100));
+
+    }
+}
+
+const PhotosUpload = {
+    uploadLimit: 6,
+    handleFilesUpload(event) {
+        const { files: filesList } = event.target;
+        const { uploadLimit } = PhotosUpload;
+
+        if (filesList.length > uploadLimit) {
+            alert(`Envie no máximo ${uploadLimit} arquivos!`);
+
+            event.preventDefault();
+
+            return
+        }
     }
 }
