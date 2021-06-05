7493ac208bedb2e95c9ca609269e762550ce8dfd
henryque <henryque_1@hotmail.com>
2021-06-05 12:11:33 -0300

    update (scripts.js and fields.njk): changing close button

--- public/scripts.js
+++ public/scripts.js
@@ -99,7 +99,7 @@ const PhotosUpload = {
   getRemoveButton() {
     const button = document.createElement('i');
     button.classList.add('material-icons');
-    button.innerHTML = "close";
+    button.innerHTML = "delete_forever";
 
     return button;
   },
