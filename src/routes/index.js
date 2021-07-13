cbc0415014c0033578d574e3e42438b78bbd062f
henryque <henryque_1@hotmail.com>
2021-07-13 16:06:00 -0300

    refactor(routes): add users and divide users and products routes

---
+++ src/routes/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const routes = express.Router();
+
+const HomeController = require("../app/controllers/HomeController");
+
+const users = require("./users");
+const products = require("./products");
+
+routes.get("/", HomeController.index);
+
+routes.use('/products', products);
+routes.use('/users', users);
+
+//ALIAS - Atalhos
+routes.get("/ads/create", (req, res) => {
+  return res.redirect('/products/create');
+});
+
+module.exports = routes;
