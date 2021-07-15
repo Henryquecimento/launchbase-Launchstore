1d3299710072e451f64736b83b3649416eb44b6c
henryque <henryque_1@hotmail.com>
2021-07-15 16:57:29 -0300

    feat(scripts): add Validade functionality to email, cpf, cnpj and cep

--- public/scripts.js
+++ public/scripts.js
@@ -184,3 +184,75 @@ const Lightbox = {
     Lightbox.closeButton.style.top = "-80px";
   }
 }
+
+const Validate = {
+  apply(input, func) {
+    Validate.clearErrors(input);
+
+    let results = Validate[func](input.value);
+    input.value = results.value;
+
+    if (results.error) {
+      Validate.displayErrors(input, results.error);
+    }
+  },
+  displayErrors(input, error) {
+    const div = document.createElement("div");
+    div.classList.add('error'),
+
+      div.innerHTML = error;
+
+    input.parentNode.appendChild(div);
+
+    input.focus();
+  },
+  clearErrors(input) {
+    const errorDiv = input.parentNode.querySelector('.error');
+
+    if (errorDiv) errorDiv.remove();
+  },
+  isEmail(value) {
+    let error = null;
+    const mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+
+    if (!value.match(mailFormat)) {
+      error = "Invalid Email";
+    }
+
+    return {
+      error,
+      value
+    }
+  },
+  isCpfCnpj(value) {
+    let error = null;
+
+    const cleanValues = value.replace(/\D/g, "");
+
+    if (cleanValues.length > 11 && cleanValues.length !== 14) {
+      error = "Invalid CNPJ";
+    } else if (value.length < 12 && cleanValues.length !== 11) {
+      error = "Invalid CPF";
+    }
+
+    return {
+      error,
+      value
+    }
+  },
+  isCep(value) {
+    let error = null;
+
+    const cleanValues = value.replace(/\D/g, "");
+
+    if (cleanValues.length !== 8) {
+      error = "Invalid CEP";
+    }
+
+    return {
+      error,
+      value
+    }
+  }
+
+}
