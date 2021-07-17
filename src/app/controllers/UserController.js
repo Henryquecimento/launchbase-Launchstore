e21680f16118fa04160f5f196c3b851d74adac2c
henryque <henryque_1@hotmail.com>
2021-07-17 18:01:59 -0300

    feat(UserController): add show method and User's create method

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,11 +1,18 @@
+const User = require('../models/User');
 
 module.exports = {
   registerFrom(req, res) {
 
     return res.render('user/register');
   },
+  show(req, res) {
+    return res.send('Ok, User signed up!');
+  },
   async post(req, res) {
 
-    return res.send('Passed!')
+    const userId = await User.create(req.body);
+
+
+    return res.redirect('/users')
   }
 }
