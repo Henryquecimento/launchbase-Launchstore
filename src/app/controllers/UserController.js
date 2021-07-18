1406ad20012e81427e70d0cba2565755b5f8f27d
henryque <henryque_1@hotmail.com>
2021-07-18 13:47:52 -0300

    refactor(UserController): readjust from class to exports the module directly

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,24 +1,6 @@
 const User = require('../models/User');
 
-class UserController {
-  registerForm(req, res) {
-
-    return res.render('user/register');
-  }
-  show(req, res) {
-    return res.send('Ok, User signed up!');
-  }
-  async post(req, res) {
-
-    const userId = await User.create(req.body);
-
-
-    return res.redirect('/users')
-  }
-}
-
-
-/* module.exports = {
+module.exports = {
   registerFrom(req, res) {
 
     return res.render('user/register');
@@ -34,6 +16,5 @@ class UserController {
     return res.redirect('/users')
   }
 }
- */
 
 module.exports = new UserController();
