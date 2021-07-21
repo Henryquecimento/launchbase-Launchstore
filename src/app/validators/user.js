c3454be8716073648a914ebcb66796c9b6b9bb61
henryque <henryque_1@hotmail.com>
2021-07-21 15:16:13 -0300

    refactor(validators/user): add checkAllFields to check filled fields

--- src/app/validators/user.js
+++ src/app/validators/user.js
@@ -1,5 +1,17 @@
 const User = require('../models/User');
 
+function checkAllFields(body) {
+  const keys = Object.keys(body);
+
+  for (key of keys) {
+    if (body[key] == "" && key) {
+      return {
+        user: body,
+        error: 'Please, You must fill all the fields!'
+      };
+    }
+  }
+}
 
 async function show(req, res, next) {
 
@@ -18,16 +30,10 @@ async function show(req, res, next) {
   next();
 }
 async function post(req, res, next) {
-  const keys = Object.keys(req.body);
 
-  for (key of keys) {
-    if (req.body[key] == "" && key) {
-      return res.render('user/register', {
-        user: req.body,
-        error: 'Please, You must fill all the fields!'
-      });
-    }
-  }
+  const fillAllFields = checkAllFields(req.body);
+
+  if (fillAllFields) return res.render('user/resgister', fillAllFields);
 
   let { email, cpf_cnpj, password, passwordRepeat } = req.body;
 
@@ -52,8 +58,12 @@ async function post(req, res, next) {
 
   next();
 }
+async function update(req, res, next) {
+
+}
 
 module.exports = {
   post,
-  show
+  show,
+  update
 }
