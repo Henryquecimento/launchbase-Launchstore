12a02986d6d73fdc3a445e879955d18b9ea27a8c
henryque <henryque_1@hotmail.com>
2021-07-22 13:39:00 -0300

    enable logout and loginForm routes

--- src/routes/users.js
+++ src/routes/users.js
@@ -5,18 +5,18 @@ const SessionController = require('../app/controllers/SessionController');
 const UserController = require('../app/controllers/UserController');
 const UserValidations = require('../app/validators/user');
 
-/* // login/logout
+// --- login/logout
 routes.get('/login', SessionController.loginForm);
-routes.post('/login', SessionController.login);
+// routes.post('/login', SessionController.login);
 routes.post('/logout', SessionController.logout);
 
-// reset password/ forgot
-routes.get('/forgot-password', SessionController.forgotForm);
-routes.get('/password-reset', SessionController.resetForm);
-routes.post('/forgot-password', SessionController.forgot);
-routes.post('/password-reset', SessionController.reset);
- */
-// User Register UserController 
+// --- reset password/ forgot
+// routes.get('/forgot-password', SessionController.forgotForm);
+// routes.get('/password-reset', SessionController.resetForm);
+// routes.post('/forgot-password', SessionController.forgot);
+// routes.post('/password-reset', SessionController.reset);
+
+// --- User Register UserController 
 routes.get('/register', UserController.registerForm);
 routes.post('/register', UserValidations.post, UserController.post);
 
