0ed979c6d4eb95bb515fddc61549f1480403907b
henryque <henryque_1@hotmail.com>
2021-07-23 16:55:27 -0300

    feat(SessionController): add nodemailer to send forgot-password email to user

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,4 +1,5 @@
 const crypto = require('crypto');
+const mailer = require('../../lib/mailer');
 const User = require('../models/User');
 
 module.exports = {
@@ -21,19 +22,48 @@ module.exports = {
   async forgot(req, res) {
     const { user } = req;
 
-    // criar token
-    const token = crypto.randomBytes(20).toString('hex');
+    try {
 
-    // criar expiração token
-    let now = new Date();
-    now = now.setHours(now.getHours() + 1);
+      // criar token
+      const token = crypto.randomBytes(20).toString('hex');
 
-    await User.update(user.id, {
-      reset_token: token,
-      reset_token_expires: now
-    })
-    // enviar email com link para recuperar senha
-    // avisar user que enviamos email
+      // criar expiração token
+      let now = new Date();
+      now = now.setHours(now.getHours() + 1);
+
+      await User.update(user.id, {
+        reset_token: token,
+        reset_token_expires: now
+      })
+      // enviar email com link para recuperar senha
+      await mailer.sendMail({
+        to: user.email,
+        from: 'no-reply@launchstore.com',
+        subject: "Password Recovery",
+        html: `<h2>Esqueceu a Senha? Perdeu a chave?</>
+      <p>Não se preocupe, clique no link abaixo para resetar a senha<p>
+      <p>
+        <a href="http://localhost:5500/users/password-reset?token=${token}" target="_blank">
+          RECUPERAR SENHA
+        </a>
+      <p>`
+      });
+
+      // avisar user que enviamos email
+      return res.render('session/forgot-password', {
+        token,
+        success: 'Verifique seu email para resetar sua senha'
+      })
+
+    } catch (err) {
+      console.error(err);
+
+      return res.render('session/forgot-password', {
+        token,
+        error: 'Erro inesperado! Tente novamente'
+      })
+    }
+
 
   },
 }
