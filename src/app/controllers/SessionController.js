47658c6e4d8ed4a560acfc9d27c3fed96ea906a7
henryque <henryque_1@hotmail.com>
2021-07-23 16:21:58 -0300

    feat(SessionController): add forgotForm, forgot methods and crypto module to create token

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,3 +1,5 @@
+const crypto = require('crypto');
+const User = require('../models/User');
 
 module.exports = {
   loginForm(req, res) {
@@ -12,5 +14,26 @@ module.exports = {
     req.session.destroy();
 
     return res.redirect('/');
-  }
+  },
+  forgotForm(req, res) {
+    return res.render('session/forgot-password');
+  },
+  async forgot(req, res) {
+    const { user } = req;
+
+    // criar token
+    const token = crypto.randomBytes(20).toString('hex');
+
+    // criar expiração token
+    let now = new Date();
+    now = now.setHours(now.getHours() + 1);
+
+    await User.update(user.id, {
+      reset_token: token,
+      reset_token_expires: now
+    })
+    // enviar email com link para recuperar senha
+    // avisar user que enviamos email
+
+  },
 }
