e42e96b695f5f4003ed6daeeda231511e364d321
henryque <henryque_1@hotmail.com>
2021-07-24 22:18:14 -0300

    feat(User): add delete method and files removal from application

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,31 +1,33 @@
 const db = require('../../config/db');
 const { hash } = require('bcryptjs');
+const Product = require('./Product');
+const fs = require('fs');
 
 module.exports = {
-    async findOne(filters) {
-        let query = `SELECT * FROM users`;
+  async findOne(filters) {
+    let query = `SELECT * FROM users`;
 
 
-        Object.keys(filters).map(key => {
+    Object.keys(filters).map(key => {
 
-            query = `
+      query = `
             ${query}
             ${key}`
 
-            Object.keys(filters[key]).map(field => {
-                query = `
+      Object.keys(filters[key]).map(field => {
+        query = `
                     ${query}
                     ${field} = '${filters[key][field]}'`
-            });
-        });
+      });
+    });
 
-        const results = await db.query(query);
+    const results = await db.query(query);
 
-        return results.rows[0];
-    },
-    async create(data) {
-        try {
-            const query = `
+    return results.rows[0];
+  },
+  async create(data) {
+    try {
+      const query = `
             INSERT INTO users (
                 name,
                 email,
@@ -36,46 +38,69 @@ module.exports = {
             ) VALUES ($1, $2, $3, $4, $5, $6)
             RETURNING id`;
 
-            const passwordHash = await hash(data.password, 8);
+      const passwordHash = await hash(data.password, 8);
 
-            const values = [
-                data.name,
-                data.email,
-                passwordHash,
-                data.cpf_cnpj.replace(/\D/g, ""),
-                data.cep.replace(/\D/g, ""),
-                data.address
-            ];
+      const values = [
+        data.name,
+        data.email,
+        passwordHash,
+        data.cpf_cnpj.replace(/\D/g, ""),
+        data.cep.replace(/\D/g, ""),
+        data.address
+      ];
 
-            const results = await db.query(query, values);
+      const results = await db.query(query, values);
 
-            return results.rows[0].id;
-        } catch (err) {
-            throw new Error(err);
-        }
-    },
-    async update(id, fields) {
-        let query = `UPDATE users SET`;
+      return results.rows[0].id;
+    } catch (err) {
+      throw new Error(err);
+    }
+  },
+  async update(id, fields) {
+    let query = `UPDATE users SET`;
 
-        Object.keys(fields).map((key, index, array) => {
+    Object.keys(fields).map((key, index, array) => {
 
-            if ((index + 1) < array.length) {
-                query = `
+      if ((index + 1) < array.length) {
+        query = `
                 ${query}
                     ${key} = '${fields[key]}',  
                 `;
-            } else {
-                //last iteration
-                query = `
+      } else {
+        //last iteration
+        query = `
                 ${query}
                     ${key} = '${fields[key]}'
                 WHERE id = ${id} 
                 `;
-            }
-        });
+      }
+    });
 
-        await db.query(query);
+    await db.query(query);
 
-        return;
-    }
+    return;
+  },
+  async delete(id) {
+    let results = await Product.find(id);
+    const productsByUser = results.rows;
+
+    const allFilesPromise = productsByUser.map(product =>
+      Product.files(product.id));
+
+    let promiseResults = await Promise.all(allFilesPromise);
+
+    promiseResults.map(results =>
+      results.rows.map(file => {
+        try {
+          fs.unlinkSync(file.path);
+
+        } catch (err) {
+          console.error(err);
+        }
+      })
+    );
+
+    await db.query(`DELETE FROM users WHERE id = $1`, [id]);
+
+  }
 }
