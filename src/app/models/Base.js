b5ff9a7d4e65d2038e34885359e65af44c1eae84
henryque <henryque_1@hotmail.com>
2021-09-19 13:10:15 -0300

    feat(models/Base): add dynamical create method

--- src/app/models/Base.js
+++ src/app/models/Base.js
@@ -40,6 +40,28 @@ const Base = {
       throw new Error('FindOne method issues');
     }
   },
+  async create(filters) {
+    let keys = [];
+    let values = [];
+
+    Object.keys(filters).map(key => {
+
+      keys.push(key)
+      values.push(filters[key]);
+
+    });
+
+    const query = `
+    INSERT INTO ${this.table}
+    (${keys.join(',')})
+    VALUES(${values.join(',')})
+    RETURNING id
+    `;
+
+    const result = await db.query(query);
+
+    return result.rows[0].id;
+  }
 }
 
 module.exports = Base;
