09e84388f37cf6e68e47d6cf2cd79e7340fec786
henryque <henryque_1@hotmail.com>
2021-09-22 18:16:32 -0300

    refactor(SearchController): readjust seach pages to handle LoadProductServices

--- src/app/controllers/SearchController.js
+++ src/app/controllers/SearchController.js
@@ -2,6 +2,7 @@ const { formatPrice } = require("../../lib/utils");
 
 const Product = require("../models/Product");
 const File = require("../models/File");
+const { LoadProduct } = require("../services/LoadProductServices");
 
 module.exports = {
   async index(req, res) {
@@ -18,23 +19,9 @@ module.exports = {
         params.category = category;
       }
 
-      async function getImage(productId) {
-        let files = await Product.files(productId);
-        files = files.map(file => `${req.protocol}://${req.headers.host}${file.path.replace("public", "")}`);
-
-        return files[0];
-      }
-
       let products = await Product.search(params);
 
-      const productsPromise = products.map(async product => {
-        product.img = await getImage(product.id);
-
-        product.oldPrice = formatPrice(product.old_price);
-        product.price = formatPrice(product.price);
-
-        return product;
-      });
+      const productsPromise = products.map(LoadProduct.format);
 
       products = await Promise.all(productsPromise);
 
