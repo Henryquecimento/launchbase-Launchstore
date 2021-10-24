58ff65f38057a2e099eb0cc25942960974755d4b
henryque <henryque_1@hotmail.com>
2021-10-24 16:56:42 -0300

    fix(scripts): readjust property name

--- public/scripts.js
+++ public/scripts.js
@@ -183,13 +183,13 @@ const Lightbox = {
   open() {
     Lightbox.target.style.opacity = 1;
     Lightbox.target.style.top = 0;
-    Lightbox.target.style.botton = 0;
+    Lightbox.target.style.bottom = 0;
     Lightbox.closeButton.style.top = 0;
   },
   close() {
     Lightbox.target.style.opacity = 0;
     Lightbox.target.style.top = "-100%";
-    Lightbox.target.style.botton = "initial";
+    Lightbox.target.style.bottom = "initial";
     Lightbox.closeButton.style.top = "-80px";
   }
 }
