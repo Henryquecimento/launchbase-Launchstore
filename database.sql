d4ba4714521b0f34c5799b35f2fc9411e79f21c9
henryque <henryque_1@hotmail.com>
2021-10-28 18:09:49 -0300

    chore: update database

--- database.sql
+++ database.sql
@@ -136,3 +136,22 @@ ALTER SEQUENCE products_id_seq RESTART WITH 1;
 ALTER SEQUENCE files_id_seq RESTART WITH 1;
 ALTER SEQUENCE orders_id_seq RESTART WITH 1;
 
+--SOFT DELETE
+
+--1. create a column in products as deleted_at
+ALTER TABLE products ADD COLUMN "deleted_at" timestamp;
+
+--2.crate a RULE that it'll run everytime we request the DELETE
+CREATE OR REPLACE RULE delete_product AS
+ON DELETE TO products DO INSTEAD
+UPDATE products
+SET deleted_at = now()
+WHERE products.id = old.id;
+
+--3.Create a VIEW where we'll take just the active data(non deleted data)
+CREATE VIEW products_without_deleted AS
+SELECT * FROM products WHERE deleted_at IS NULL;
+
+--4.RENAME THE VIEW AND THE PRODUCT TABLE
+ALTER TABLE products RENAME TO products_with_deleted;
+ALTER VIEW products_without_deleted RENAME TO products;
